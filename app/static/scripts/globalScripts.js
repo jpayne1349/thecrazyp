2b6bdab5835a7fd614046792f3afa1e7d37486ca
James Payne <james.payne1349@gmail.com>
2021-07-11 16:51:56 -0500

    mmockup of a homepage made. pictures pulled from instagram

--- app/static/scripts/globalScripts.js
+++ app/static/scripts/globalScripts.js
@@ -1,5 +1,22 @@
 
-let today = new Date();
 
-console.log(today);
 
+var navbar = document.getElementById('navbar');
+var navbar_link_holder = document.getElementById('navbar_link_holder');
+var screen_width = screen.width;
+var navbar_links = document.getElementsByClassName('navbar_link');
+
+console.log('screen width', screen_width);
+if( screen.width < 600 ) {
+    
+    navbar_link_holder.addEventListener('click', () => {
+        for( let link of navbar_links ) {
+            if(link.classList.contains('show')) {
+                link.classList.remove('show');
+            } else {
+                link.classList.add('show');
+            }
+        }
+    });
+    
+}
