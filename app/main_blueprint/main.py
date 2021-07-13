82b10947bfbef689261ac9049f9770a1285856c8
James <james.payne1349@gmail.com>
2021-07-13 08:02:49 -0500

    building the photo carousel on the homepage. this is a mockup to get started. need to refine.

--- app/main_blueprint/main.py
+++ app/main_blueprint/main.py
@@ -3,9 +3,28 @@ from flask import Blueprint, render_template, flash
 
 from flask import current_app as app
 
+import os, json
+
 main_blueprint = Blueprint('main_blueprint', __name__) 
 
 
 @main_blueprint.route('/')
 def homepage():
     return render_template('homepage.html')
+
+@main_blueprint.route('/carousel_photos', methods=['POST'])
+def carousel_photos():
+
+    static = 'static'
+    photos = 'carousel_photos'
+
+    blueprint_dir = os.path.dirname(__file__)
+    app_dir = os.path.dirname(blueprint_dir)
+    
+    photos_dir = os.path.join(app_dir, static, photos)
+    
+    list_of_photos = os.listdir(photos_dir)
+
+    photos_list = json.dumps(list_of_photos)
+
+    return photos_list
--- app/static/scripts/globalScripts.js
+++ app/static/scripts/globalScripts.js
@@ -6,7 +6,8 @@ var navbar_link_holder = document.getElementById('navbar_link_holder');
 var screen_width = screen.width;
 var navbar_links = document.getElementsByClassName('navbar_link');
 
-console.log('screen width', screen_width);
+
+// responsive navbar, run right away
 if( screen.width < 600 ) {
     
     navbar_link_holder.addEventListener('click', () => {
@@ -19,4 +20,93 @@ if( screen.width < 600 ) {
         }
     });
     
+}
+
+
+// fetching a list of photo extensions and passing to element creation
+fetch('/carousel_photos', { method: 'POST' })
+    .then(response => response.json())
+    .then(json => populate_carousel(json))
+    .catch( error => console.log('ERROR', error));
+
+// creating img's and assigning sources..
+function populate_carousel(photo_names) {
+    var carousel_container = document.getElementById('picture_carousel');
+    var indicator_container = document.getElementById('carousel_indicator');
+    var file_string = 'static/carousel_photos/';
+
+    console.log(carousel_container);
+
+    for( let photo_name of photo_names ) {
+        
+        var photo_url = file_string + photo_name
+        console.log(photo_url);
+
+        var image_container = document.createElement('div');
+        image_container.className = 'image_container';
+
+        var new_carousel_img = document.createElement('img');
+        new_carousel_img.className = 'ss_picture';
+        new_carousel_img.src = photo_url;
+
+        image_container.append(new_carousel_img);
+        
+        carousel_container.append(image_container);
+
+        // indicator dot
+        var new_indicator = document.createElement('div');
+        new_indicator.className = 'ss_indicator';
+        new_indicator.setAttribute('for', photo_name);
+
+        indicator_container.append(new_indicator);
+    }
+    
+    carousel_movement();
+}
+
+
+
+function carousel_movement() {
+    var carousel_container = document.getElementById('picture_carousel');
+
+    var carousel_cont_center = carousel_container.offsetWidth / 2;
+
+    var zero_center = carousel_container.offsetLeft + 15;
+    
+    var photo_containers = document.getElementsByClassName('image_container');
+
+    var image_cont_center =  photo_containers[0].offsetWidth / 2;
+
+    var first_center = carousel_cont_center - image_cont_center;
+
+    // calculate translation points..
+    var center_points = [];
+    let image_count = photo_containers.length;
+    let i;
+    for(i = 0; i < image_count; i++ ) {
+        center_points.push(first_center - (i * photo_containers[0].offsetWidth)); 
+        // set initial translate
+        photo_containers[i].style.translate = first_center + 'px';  
+    }
+    console.log(center_points);
+
+    // photo_containers[0].style.translate = center_points[0] +'px';
+
+
+    var counter = 1;
+    var move_on_interval = window.setInterval( function() {
+        for( let photo of photo_containers ) {
+            photo.style.translate = center_points[counter] + 'px';
+
+        }
+        counter = counter + 1;
+
+        if(counter > photo_containers.length) {
+            counter = 0;
+        }
+
+        
+
+    }, 3000);
+
 }
