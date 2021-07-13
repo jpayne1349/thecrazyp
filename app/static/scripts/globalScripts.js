128879dc81b0080605613c61b11e6b6657effb5b
James <james.payne1349@gmail.com>
2021-07-13 12:38:28 -0500

    carousel v1 done. no interactivity yet. just smooth transitioning.

--- app/static/scripts/globalScripts.js
+++ app/static/scripts/globalScripts.js
@@ -79,5 +79,75 @@ function populate_carousel(list_of_photo_file_names) {
 
 function carousel_animation(carousel_object) {
     // set up the interval for changing the classes..
+    let interval_time = 5000;
+
+    // initialize at zero
+    update_animation(carousel_object, 0);
+
+    // testing this, dumb way to change a css class
+    for(let style_object of document.styleSheets[0].cssRules) {
+        if(style_object.selectorText == '.prev') {
+            setTimeout( () => {
+                style_object.style.cssText = "transform: translate(-85vw); opacity: 0.5; transition: transform 1s ease-in, opacity 1s;";
+            }, 1000);
+        }
+    }
+
+    // for tracking 
+    let active_index = 1;
+
+    console.log(carousel_object);
+
+    window.setInterval(() => {
+        console.log(active_index);
+
+        update_animation(carousel_object, active_index);
+
+        active_index += 1;
+        if ( active_index == carousel_object.pictures.length ) active_index = 0;
+
+    }, interval_time);
+
 
 }
+
+function update_animation(carousel_object, active_index) {
+
+    // loop through the object to set classes on everything
+    let index = 0;
+    for( index = 0; index < carousel_object.pictures.length; index++ ) {
+        if(index == active_index) {
+            carousel_object.pictures[index].className = 'image_container active';
+            carousel_object.indicators[index].className = 'ss_indicator filled';
+        } else if( index == active_index - 1 ) {
+            // previous
+            carousel_object.pictures[index].className = 'image_container prev';
+            if(carousel_object.indicators[index].classList.contains('filled')) {
+                carousel_object.indicators[index].classList.remove('filled');
+            }
+        } else if (index == active_index + 1 ) {
+            // next
+            carousel_object.pictures[index].className = 'image_container next';
+        } else {
+            // all others
+            carousel_object.pictures[index].className = 'image_container';
+            if(carousel_object.indicators[index].classList.contains('filled')) {
+                carousel_object.indicators[index].classList.remove('filled');
+            }
+        }
+
+        // additional settings for beginning and end
+        if( active_index == 0 ) {
+            carousel_object.pictures[carousel_object.pictures.length - 1].classList.add('prev');
+
+        }
+
+        if( active_index == carousel_object.pictures.length - 1) {
+            carousel_object.pictures[0].classList.add('next');
+        }
+
+    }
+
+
+}
+
