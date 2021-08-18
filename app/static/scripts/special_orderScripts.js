caf4d400121b4ce39527a16790a74c2cffe6b7eb
James Payne <james.payne1349@gmail.com>
2021-08-18 00:11:51 -0500

    styled up the Other option

--- app/static/scripts/special_orderScripts.js
+++ app/static/scripts/special_orderScripts.js
@@ -3,6 +3,19 @@
 main();
 
 function main() {
+    show_first_selection();
+
+}
+
+async function show_first_selection() {
+
+    const first_selection = await init_label_holders();
+    const other_options = await init_other_options();
+    first_selection.click();
+
+}
+
+function init_label_holders() {
 
     let label_holders = document.getElementsByClassName('label_holder');
     let index_counter = 0;
@@ -26,11 +39,13 @@ function main() {
         
         label_holders[i].addEventListener('click', () => {
             if( selection_below.classList.contains('show')) {
+                label_holders[i].classList.remove('focus');
                 selection_below.classList.remove('show');
                 arrow.classList.remove('showing');
                 wrapper.classList.remove('expanded');
                 wrapper.style.height = orig_wrapper_ht + 'px';
             } else {
+                label_holders[i].classList.add('focus');
                 selection_below.classList.add('show');
                 arrow.classList.add('showing');
                 wrapper.classList.add('expanded');
@@ -41,5 +56,67 @@ function main() {
 
     }
 
-    window.addEventListener('click', () => console.log(event.target));
+    return label_holders[0];
+
+}
+
+function init_other_options() {
+    let other_options = document.getElementsByClassName('other');
+
+    // all options need an event listener for selection...
+    // also needs to trigger a removal for any other option previously selected
+    // check the associated children. possibly reset the 'other' block
+    for(let n = 0; n < other_options.length; n++) {
+        other_options[n].addEventListener('click', () => {
+            // expand the other input
+            other_options[n].classList.add('focus');
+            let close = other_options[n].firstElementChild;
+            let label = close.nextElementSibling;
+            let input = label.nextElementSibling;
+            input.classList.add('show');
+            close.classList.add('show');
+
+            let parent_holder = other_options[n].parentElement;
+            let options = parent_holder.children;
+            for(let i = 0; i < options.length - 1; i++ ) {
+                options[i].classList.add('hide');
+            }
+
+            close.addEventListener('click', () => {
+                // don't trigger the outer click listener too.
+                event.stopPropagation();
+                if(input.classList.contains('show')) {
+                    input.classList.remove('show');
+                    close.classList.remove('show');
+                    other_options[n].classList.remove('focus');
+                    for(let i = 0; i < options.length - 1; i++ ) {
+                        options[i].classList.remove('hide');
+                    }
+                }
+            });
+
+            input.addEventListener('input', function() {
+                console.log(event);
+                if(input.value == '') { // if no text in
+                    if(other_options[n].classList.contains('value_added')) {
+                        other_options[n].classList.remove('value_added');
+                        close.classList.remove('accept_input');
+                        label.innerText = 'Other';
+                    }
+                } else { // found text
+                    if(other_options[n].classList.contains('value_added')) {   
+                        label.innerText = input.value;
+                    } else {
+                        other_options[n].classList.add('value_added');
+                        close.classList.add('accept_input');
+                        label.innerText = input.value;
+                    }
+
+                }
+                
+            });
+        });
+
+
+    }
 }
