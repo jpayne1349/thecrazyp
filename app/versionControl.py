1a5e7699b3a61b831a39eabe3d456f17862e9798
James <james.payne1349@gmail.com>
2021-10-04 12:09:07 -0500

    fixes #2 new python script to help with auto updates of the html version tags to be run from the command Line prompts user to input a new version , such as 06 this changes all the html src and script files with ?v= sections to a new ?v=06 entry

---
+++ app/versionControl.py
@@ -0,0 +1,58 @@
+
+
+
+# goal of this module is to run a file search and change the static files appended version numbers automatically...
+
+# vers 0.1 of the versionControl.. just search all the templates, and prompt the user to input a new version number, then set all
+
+import re
+import os
+from bs4 import BeautifulSoup # this is beautifulSoup for easy file manip of html
+
+regex_for_v = "\?v=(\d+)"
+
+# currently share folder with 'templates' directory
+
+def updateVersion():
+
+    user_input_version_number = input('What version number?')
+
+    new_version = '?v=' + user_input_version_number
+
+    templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
+    
+    templates_file_list = os.listdir(templates_dir)
+
+    for template in templates_file_list:
+        templ_filepath = os.path.join(templates_dir, template)
+        with open(templ_filepath) as current_file:
+            
+            soup = BeautifulSoup(current_file, 'html.parser')
+            
+            link = soup.link
+            if link:
+                print('OLD', link['href'])
+                # check here for a version in this href
+                old_href = link['href']
+                new_href = re.sub(regex_for_v, str(new_version), old_href)
+                if new_href: 
+                    print('NEW',new_href)
+                    soup.link['href'] = new_href
+
+            script = soup.script
+            if script:
+                print('OLD', script['src'])
+                old_src = script['src']
+                new_src = re.sub(regex_for_v, str(new_version), old_src)
+                if new_src:
+                    print('NEW', new_src)
+                    soup.script['src'] = new_src
+        
+        with open(templ_filepath, "w") as new_file:
+            new_file.write(str(soup))
+            
+    
+    return 
+
+if __name__ == '__main__':
+    updateVersion()
